dfb61a8a9d1764559151cc118723400697063901
Maiengelei <Maiengelei@outlook.com>
2023-05-05 19:40:25 +0800

    修改Lottery Data数据源

--- Assets/Scripts/Lottery/LotteryData.cs
+++ Assets/Scripts/Lottery/LotteryData.cs
@@ -9,12 +9,23 @@ namespace Lottery
     public class LotteryData : MonoBehaviour
     {
         [Serializable]
-        private struct LotteryDataBase
+        public struct LotteryDataBase
         {
             public Item item;
             public float weighted;
         }
 
-        [SerializeField] private List<LotteryDataBase> lotteryList;
+        [SerializeField] 
+        List<LotteryDataBase> lotteryList;
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        public List<LotteryDataBase> GetLotteryList => lotteryList;
+
+        public Item GetRandomItem()
+        {
+            return null;
+        }
     }
 }
