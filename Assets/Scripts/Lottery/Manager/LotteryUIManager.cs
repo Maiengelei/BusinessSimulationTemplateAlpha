87e16e8dfecd8d06e89113dc165bedf1f4395458
Maiengelei <Maiengelei@outlook.com>
2023-05-05 19:41:23 +0800

    增加LotteryUI管理类，并新增UpdateUI方法

---
+++ Assets/Scripts/Lottery/Manager/LotteryUIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Lottery.Manager
+{
+    public class LotteryUIManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 抽奖机对象
+        /// </summary>
+        public GameObject lottery;
+
+        /// <summary>
+        /// 抽奖机格子父对象
+        /// </summary>
+        public Transform lotteryGrid;
+
+        /// <summary>
+        /// 抽奖机格子预制体
+        /// </summary>
+        public GameObject lotterySlotPrefab;
+
+        /// <summary>
+        /// 接受 LotteryList
+        /// </summary>
+        private LotteryData _lotteryList;
+
+        private void OnEnable()
+        {
+            // 启用UI时获取抽奖机奖池
+            _lotteryList = lottery.GetComponent<LotteryData>();
+
+            // 启动时更新界面
+            UpdateGrid();
+        }
+
+        /// <summary>
+        /// 更新抽奖机奖品界面
+        /// </summary>
+        private void UpdateGrid()
+        {
+            foreach (var item in _lotteryList.GetLotteryList)
+            {
+                var itemObj = Instantiate(lotterySlotPrefab,lotteryGrid);
+                itemObj.transform.Find("ItemSprite").GetComponent<Image>().sprite = item.item.itemSprite;
+            }
+        }
+    }
+}
