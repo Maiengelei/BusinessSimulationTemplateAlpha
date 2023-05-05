2edcee1b81828f4698d56d2c2701ab572e66e77b
Maiengelei <Maiengelei@outlook.com>
2023-05-05 12:05:49 +0800

    测试代码

---
+++ Assets/Scripts/Test/BagAddItem.cs
@@ -0,0 +1,27 @@
+﻿using Inventory.InventoryObject;
+using Player;
+using UnityEngine;
+
+namespace Test
+{
+    public class BagAddItem : MonoBehaviour
+    {
+        private Inventory.InventoryObject.Inventory _inventory;
+        public Item item;
+        private void Awake()
+        {
+            // 通过 Tag 获取玩家对象
+            GameObject player = GameObject.FindWithTag("Player");
+            // 获取到玩家的背包
+            _inventory = player.GetComponent<GamerObjectProperties>().Inventory;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                _inventory.AddItemToList(item);
+            }
+        }
+    }
+}
