141d9b6a430975bc4559668d6144b95a53b8a9b4
Maiengelei <Maiengelei@outlook.com>
2023-05-05 10:35:47 +0800

    新增玩家对象属性类，用于获取玩家的预设数据，如背包

---
+++ Assets/Scripts/Player/GamerObjectProperties.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class GamerObjectProperties : MonoBehaviour
+    {
+        [SerializeField] private Inventory.InventoryObject.Inventory inventory;
+        
+        public Inventory.InventoryObject.Inventory Inventory => inventory;
+    }
+}
