2555a3ac1c2b67bf429354d4e98c0c580e753f79
Maiengelei <Maiengelei@outlook.com>
2023-05-06 11:48:49 +0800

    增加玩家进入/离开区域时的处理

--- Assets/Scripts/Lottery/Manager/LotteryManager.cs
+++ Assets/Scripts/Lottery/Manager/LotteryManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using Inventory.InventoryObject;
+using Player;
+using Unity.VisualScripting;
+using UnityEngine;
 
 /*
  * 1. 通过box碰撞来获得进去区域的玩家
@@ -11,17 +14,84 @@ namespace Lottery.Manager
 {
     public class LotteryManager : MonoBehaviour
     {
-        public delegate void LotteryPlayerDelegate(Collider other);
-        public event LotteryPlayerDelegate PlayerStatus;
+        /// <summary>
+        /// 抽奖机的UI界面 旋转父物体
+        /// </summary>
+        public Transform uiRotatePoint;
 
-        private void OnTriggerEnter(Collider other)
+        /// <summary>
+        /// 抽奖机碰撞组件
+        /// </summary>
+        public LotteryTrigger colliderTrigger;
+
+        // ---------------------------------------------------------------
+
+        /// <summary>
+        /// 玩家背包
+        /// </summary>
+        private InventoryObject _player;
+
+        /// <summary>
+        /// 抽奖机数据存储对象
+        /// </summary>
+        private LotteryData _lotteryData;
+
+        // ---------------------------------------------------------------
+
+        private void OnEnable()
         {
-            if (other.gameObject.CompareTag("Player"))
+            _lotteryData = GetComponent<LotteryData>();
+
+            colliderTrigger.PlayerTriggerEnter += PlayerEnter;
+            colliderTrigger.PlayerTriggerExit += PlayerExit;
+        }
+
+        private void OnDestroy()
+        {
+            colliderTrigger.PlayerTriggerEnter -= PlayerEnter;
+            colliderTrigger.PlayerTriggerExit -= PlayerExit;
+        }
+
+        // ---------------------------------------------------------------
+
+        /// <summary>
+        /// 当玩家进入 Trigger 时
+        /// </summary>
+        /// <param name="other">玩家</param>
+        private void PlayerEnter(Collider other)
+        {
+            _player = other.GameObject()
+                .GetComponent<GamerObjectProperties>()
+                .InventoryObject;
+            uiRotatePoint.gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// 当玩家离开 Trigger 时
+        /// </summary>
+        /// <param name="other">玩家</param>
+        private void PlayerExit(Collider other)
+        {
+            _player = null;
+            uiRotatePoint.gameObject.SetActive(false);
+        }
+
+        // ---------------------------------------------------------------
+
+        /// <summary>
+        /// 向玩家背包中新增道具
+        /// </summary>
+        private void GetLotteryItem()
+        {
+            // 检查玩家背包是否有足够的空间
+            if (_player != null && _player.CheckItemList())
             {
-                if (PlayerStatus != null)
-                {
-                    PlayerStatus(other);
-                }
+                var item = _lotteryData.GetRandomItem();
+                _player.AddItemToList(item);
+            }
+            else
+            {
+                Debug.Log($"你的背包满了，背包最大容量为{_player.inventoryLenght}");
             }
         }
     }
