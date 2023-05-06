7d453299a847c2742985fd2ad8955ec2985c5f1d
Maiengelei <Maiengelei@outlook.com>
2023-05-06 11:50:11 +0800

    增加触发器委托

---
+++ Assets/Scripts/Lottery/Manager/LotteryTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Lottery.Manager
+{
+    public class LotteryTrigger : MonoBehaviour
+    {
+        public delegate void LotteryPlayerDelegate(Collider other);
+
+        public event LotteryPlayerDelegate PlayerTriggerEnter;
+        public event LotteryPlayerDelegate PlayerTriggerExit;
+
+        // ---------------------------------------------------------------
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (PlayerTriggerEnter != null)
+                {
+                    PlayerTriggerEnter(other);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (PlayerTriggerExit != null)
+                {
+                    PlayerTriggerExit(other);
+                }
+            }
+        }
+    }
+}
