8940726f208a1e3c396a5343df64b57c53244e0e
Maiengelei <Maiengelei@outlook.com>
2023-05-06 11:51:17 +0800

    修改命名

--- Assets/Scripts/Test/BagAddItem.cs
+++ Assets/Scripts/Test/BagAddItem.cs
@@ -6,21 +6,21 @@ namespace Test
 {
     public class BagAddItem : MonoBehaviour
     {
-        private Inventory.InventoryObject.Inventory _inventory;
-        public Item item;
+        private InventoryObject _inventoryObject;
+        public ItemObject itemObject;
         private void Awake()
         {
             // 通过 Tag 获取玩家对象
             GameObject player = GameObject.FindWithTag("Player");
             // 获取到玩家的背包
-            _inventory = player.GetComponent<GamerObjectProperties>().Inventory;
+            _inventoryObject = player.GetComponent<GamerObjectProperties>().InventoryObject;
         }
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.I))
             {
-                _inventory.AddItemToList(item);
+                _inventoryObject.AddItemToList(itemObject);
             }
         }
     }
