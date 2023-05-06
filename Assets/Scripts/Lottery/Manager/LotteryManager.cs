0aff74d29b0d1ea9f000d99d65b530888067832b
Maiengelei <Maiengelei@outlook.com>
2023-05-06 20:09:54 +0800

    移除注释

--- Assets/Scripts/Lottery/Manager/LotteryManager.cs
+++ Assets/Scripts/Lottery/Manager/LotteryManager.cs
@@ -3,13 +3,6 @@ using Player;
 using Unity.VisualScripting;
 using UnityEngine;
 
-/*
- * 1. 通过box碰撞来获得进去区域的玩家
- * 2. 当进去区域的玩家为Player时，为其打开抽奖按钮
- * 3. 当点击抽奖时，先检查玩家背包是否已经满了
- * 3.1 如果满了则提示玩家背包已满
- * 3.2 如果有空位则开始抽奖
- */
 namespace Lottery.Manager
 {
     public class LotteryManager : MonoBehaviour
