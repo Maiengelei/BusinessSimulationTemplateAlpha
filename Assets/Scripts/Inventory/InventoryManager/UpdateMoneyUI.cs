9d1178540a299ff78f3966c596ffdbcaff375d2b
Maiengelei <Maiengelei@outlook.com>
2023-05-11 20:58:34 +0800

    增加MoneyUI刷新功能

--- Assets/Scripts/Inventory/InventoryManager/UpdateMoneyUI.cs
+++ Assets/Scripts/Inventory/InventoryManager/UpdateMoneyUI.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using Inventory.InventoryScriptableObject;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Inventory.InventoryManager
 {
     public class UpdateMoneyUI : MonoBehaviour
     {
+        [SerializeField] private InventoryObject inventoryObject;
+        [SerializeField] private TextMeshProUGUI moneyText;
+
+        private void OnEnable()
+        {
+            moneyText.text = inventoryObject.GetMoney().ToString();
+            inventoryObject.UpdateMoneyUI += UpdateUIText;
+        }
         
+        private void OnDestroy()
+        {
+            inventoryObject.UpdateMoneyUI += UpdateUIText;
+        }
+
+        private void UpdateUIText()
+        {
+            moneyText.text = inventoryObject.GetMoney().ToString();
+        }
     }
 }
