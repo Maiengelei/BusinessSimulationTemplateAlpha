dbd845baceb645f1b06e5241b6e11161e3e03475
Maiengelei <Maiengelei@outlook.com>
2023-05-12 12:08:03 +0800

    修改跟踪方式和元素管理方式

--- Assets/Scripts/Pet/PetManager.cs
+++ Assets/Scripts/Pet/PetManager.cs
@@ -3,24 +3,24 @@
  */
 
 
-using System;
 using System.Collections.Generic;
 using Player;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Pet
 {
     public class PetManager : MonoBehaviour
     {
-        private GameObject _player; // 玩家对象
-        public float distance; // 预制件之间的距离
-
-        private List<GameObject> _prefabList = new List<GameObject>();
+        private GameObject _player;
+        public float followDistance = 1.0f;
+        private List<GameObject> _pets = new List<GameObject>();
+        public float petSpacing = 1.0f;
 
         private void OnEnable()
         {
             _player = GameObject.FindWithTag("Player");
-            
+
             _player.GetComponent<GamerObjectProperties>().InventoryObject.PetAddList += CreatePrefab;
             _player.GetComponent<GamerObjectProperties>().InventoryObject.PetRemoveList += DeletePrefab;
         }
@@ -39,33 +39,42 @@ namespace Pet
         // 用于实例化预制件并将其添加到列表中
         private void CreatePrefab(GameObject prefabObject)
         {
-            // 计算新预制件的位置
-            Vector3 pos = _player.transform.position + new Vector3(_prefabList.Count * distance, 0, 0);
-
-            // 实例化新预制件
-            GameObject obj = Instantiate(prefabObject, pos, Quaternion.identity, transform);
-
-            // 将新预制件添加到列表中
-            _prefabList.Add(obj);
+            GameObject newPet = Instantiate(prefabObject,
+                _player.transform.position - new Vector3(0, 0, petSpacing * (_pets.Count + 1)), Quaternion.identity);
+            newPet.GetComponent<PetObject>().Initialize(_player, new Vector3(0, 0, -petSpacing * (_pets.Count + 1)));
+            _pets.Add(newPet);
         }
 
         // 更新预制件的位置，使其跟随玩家
         private void UpdatePrefabPositions()
         {
-            for (int i = 0; i < _prefabList.Count; i++)
+            if (_pets.Count > 0)
             {
-                Vector3 newPos = _player.transform.position + new Vector3(i * distance, 0, 0);
-                _prefabList[i].transform.position = newPos;
+                float distance = Vector3.Distance(_player.transform.position, _pets[0].transform.position);
+                if (distance > followDistance)
+                {
+                    for (int i = 0; i < _pets.Count; i++)
+                    {
+                        _pets[i].GetComponent<PetObject>().offset = new Vector3(0, 0, -petSpacing * (i + 1));
+                    }
+                }
             }
         }
 
         private void DeletePrefab(int index)
         {
-            // 如果该元素存在
-            if (index < _prefabList.Count)
+            if(index < 0 || index >= _pets.Count)
             {
-                // 移除该元素
-                _prefabList.RemoveAt(index);
+                return;
+            }
+
+            GameObject petToRemove = _pets[index];
+            _pets.RemoveAt(index);
+            Destroy(petToRemove);
+
+            for(int i = index; i < _pets.Count; i++)
+            {
+                _pets[i].GetComponent<PetObject>().offset = new Vector3(0, 0, -petSpacing * (i + 1));
             }
         }
     }
